0158b20c4292512ffc83ba543c4549b5bf89f705
lhj <2830935595@qq.com>
2023-11-30 14:40:10 +0800

    homework_c1

---
+++ src/homework/Cipher.java
@@ -0,0 +1,131 @@
+package homework;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Cipher {
+    //创建三个变量
+    String plaintext, key, ciphertext;
+
+    //构造方法给变量赋值
+    public Cipher() {
+        plaintext = "202210139075"; //初始明文
+        ciphertext = "32566625<457";//初始密文
+        key = "123456";             //初始密匙
+    }
+
+    //设置密匙
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    //获得密匙方法
+    public String getKey() {
+        return key;
+    }
+
+    //输入明文方法
+    public void setPlaintext(String plaintext) {
+        this.plaintext = plaintext;
+        System.out.println("明文设置成功");
+    }
+
+    //获得明文方法
+    public String getPlaintext() {
+        return plaintext;
+    }
+
+    //输入密文方法
+    public void setCiphertext(String ciphertext) {
+        this.ciphertext = ciphertext;
+        System.out.println("密文设置成功");
+    }
+
+    //获得密文方法
+    public String getCiphertext() {
+        return ciphertext;
+    }
+
+    //创建动态数组储存数据
+    ArrayList<Character> c1 = new ArrayList<Character>();
+
+    //加密算法
+    public String encryption(String temp) {
+        String tp = "";                     //创建临时变量tp接受加密后的密文
+        for (int i = 0; i < temp.length(); i++) {
+            int p = temp.charAt(i) - 0;       //获取明文中每个字符的Ascll码
+            if (p >= 32 && p <= 126) {             //若果明文为可见字符
+                for (int j = 0; j < key.length(); j++) {
+                    p += key.charAt(j) - '0';      //明文Ascll码循环加上密匙加密
+                    if (p > 126) {                 //若字符Ascll码大于126(不可见字符)
+                        p = p % 126 + 31;
+                    }
+                }
+            }
+            c1.add((char) (p));                    //将加密后的Ascll码转成字符加入动态数组
+        }
+        for (int j = 0; j < c1.size(); j++) {
+            tp += String.valueOf(c1.get(j));   //循环动态数组将字符串拼接并传输给临时变量tp
+        }
+        c1.clear();                                //清空动态数组（因为动态数组多次使用，需要清除遗留的数据，避免影响下一次使用）
+        return tp;                                 //返回密文
+    }
+
+    //解密算法
+    public String decryption(String temp) {
+        String tp = "";                          //创建临时变量tp接受解密后的明文
+        for (int i = 0; i < temp.length(); i++) {
+            int p = temp.charAt(i) - 0;          //获取密文中每个字符的Ascll码
+            if (p >= 32 && p <= 126) {                 //若果密文为可见字符
+                for (int j = 0; j < key.length(); j++) {//循环密匙进行解密
+                    p -= (key.charAt(j) - '0');         //密文字符的Ascll码减去密匙的每个字符的字面量值
+                    if (p < 32) {                       //若字符Ascll码小于36(不可见字符)
+                        p = p - 31 + 126;               //还原Ascll码值
+                    }
+                }
+            }
+            c1.add((char) (p));                      //解密后的Ascll码转成字符加入动态数组
+        }
+        for (int j = 0; j < c1.size(); j++) {
+            tp += String.valueOf(c1.get(j)); //循环动态数组将字符串拼接并传输给临时变量tp
+        }
+        c1.clear();                                //清空动态数组（因为动态数组多次使用，需要清除遗留的数据，避免影响下一次使用）
+        return tp;                                 //返回明文
+    }
+
+    //对文件进行加密
+    public String encryption1() throws IOException {
+        Reader reader = new FileReader("plain.txt");//创建字节输入流，指向plain.txt文件
+        StringBuilder temp = new StringBuilder();   //用于储存明文
+        int data;
+        while ((data=reader.read()) != -1) {
+            temp.append((char) data);  //获取文件内容追加到字符串temp
+        }
+        String str = temp.toString();           //StringBuilder转成String
+        reader.close();                         //关闭字节输入流
+        String en_str = encryption(str);        //将加密后的内容重新赋值给en_str变量
+
+        Writer writer = new FileWriter("cipher.txt");//加密的内容写到cipher.txt文件
+        writer.write(en_str);
+        writer.close();
+        return en_str;
+    }
+
+    //对文件进行解密
+    public String decryption1() throws IOException {
+        Reader reader = new FileReader("cipher.txt");//创建字节输入流，指向cipher.txt文件
+        StringBuilder temp = new StringBuilder();   //用于储存密文
+        int data;
+        while ((data=reader.read()) != -1) {
+            temp.append((char) data);  //获取文件内容追加到字符串temp
+        }
+        String str = temp.toString();           //StringBuilder转成String
+        reader.close();
+        String en_str = decryption(str);        //将解密后的内容重新赋值给en_str变量
+
+        Writer writer = new FileWriter("plain.txt");//加密的内容写到plain.txt文件
+        writer.write(en_str);
+        writer.close();
+        return en_str;
+    }
+}
---
+++ src/homework/CipherGUI.java
@@ -0,0 +1,164 @@
+package homework;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+public class CipherGUI {
+    private JFrame frame;//创建窗口变量
+    private JButton setKeyButton;//创建设置密匙按钮变量
+    private JButton setPlaintextButton;//创建设置明文按钮变量
+    private JButton setCiphertextButton;//创建设置密文按钮变量
+    private JButton encryptButton;//创建加密按钮变量
+    private JButton decryptButton;//创建解密按钮变量
+    private JButton encryptFileButton;//创建加密文件按钮变量
+    private JButton decryptFileButton;//创建解密文件按钮变量
+    private JButton exitButton;//创建退出程序按钮变量
+
+    //创建接收密码、明文、密文文本框变量
+    private JTextField keyField;
+    private JTextField plaintextField;
+    private JTextField ciphertextField;
+
+    //创建Cipher类变量
+    private Cipher cipher;
+
+    //创建ifValidKey()方法判断密匙输入位数是否合法
+    private boolean ifValidKey(String key) {
+        return key.length() >= 6 && key.length() <= 12;
+    }
+
+    //构造方法,给变量赋值
+    public CipherGUI() {
+        frame = new JFrame("李杭基加密解密系统");//创建标题为加解密系统窗口
+        frame.setSize(370, 300);//窗口初始大小
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//点击X关闭窗口
+        frame.setResizable(false);//固定窗口大小
+        cipher = new Cipher();
+
+        //给各个按钮赋值
+        setKeyButton = new JButton("设置密钥");
+        setPlaintextButton = new JButton("设置明文");
+        setCiphertextButton = new JButton("设置密文");
+        encryptButton = new JButton("加密");
+        decryptButton = new JButton("解密");
+        encryptFileButton = new JButton("加密文件");
+        decryptFileButton = new JButton("解密文件");
+        exitButton = new JButton("退出");
+
+        //给文本框赋值
+        keyField = new JTextField(cipher.getKey(), 20);
+        plaintextField = new JTextField(cipher.getPlaintext(), 20);
+        ciphertextField = new JTextField(cipher.getCiphertext(), 20);
+
+        //通过匿名的方式创建监听器，使事件发生
+        setKeyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String key = keyField.getText();
+                if (ifValidKey(key)) {//判断密匙设置是否合法，只有合法其他按钮才可以激活，才能继续操作
+                    cipher.setKey(key);
+                    //启用其他按钮
+                    setPlaintextButton.setEnabled(true);
+                    setCiphertextButton.setEnabled(true);
+                    encryptButton.setEnabled(true);
+                    decryptButton.setEnabled(true);
+                    encryptFileButton.setEnabled(true);
+                    decryptFileButton.setEnabled(true);
+                    JOptionPane.showMessageDialog(frame,"设置成功！","密码设置",JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    // 密钥不合法，禁用其他按钮
+                    setPlaintextButton.setEnabled(false);
+                    setCiphertextButton.setEnabled(false);
+                    encryptButton.setEnabled(false);
+                    decryptButton.setEnabled(false);
+                    encryptFileButton.setEnabled(false);
+                    decryptFileButton.setEnabled(false);
+                    JOptionPane.showMessageDialog(frame,"密码设置不合法，请重新设置！","密码设置",JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+
+        setPlaintextButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String plaintext = plaintextField.getText();//接受文本框的内容
+                cipher.setPlaintext(plaintext);//设置明文
+                JOptionPane.showMessageDialog(frame,"明文设置成功");//弹出提示窗口
+            }
+        });
+
+        setCiphertextButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String ciphertext = ciphertextField.getText();
+                cipher.setCiphertext(ciphertext);
+                JOptionPane.showMessageDialog(frame,"密文设置成功");
+            }
+        });
+
+        encryptButton.addActionListener(new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                String str = cipher.encryption(cipher.plaintext);
+                ciphertextField.setText(str);//密文文本框被设置成当前加密后的密文
+                cipher.setCiphertext(str);   //密文被设置为当前加密的密文
+                JOptionPane.showMessageDialog(null,cipher.encryption(cipher.plaintext),"加密成功",JOptionPane.INFORMATION_MESSAGE);//弹出消息提示框，展示加密后的密文
+            }
+        });
+
+        decryptButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String str = cipher.decryption(cipher.ciphertext);
+                plaintextField.setText(str);//明文文本框被设置成当前解密后的明文
+                cipher.setPlaintext(str);   //明文被设置成当前解密后的明文
+                JOptionPane.showMessageDialog(null,cipher.decryption(cipher.ciphertext),"解密成功",JOptionPane.INFORMATION_MESSAGE);//弹出消息提示框，展示解密后的明文
+            }
+        });
+
+        encryptFileButton.addActionListener(new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    JOptionPane.showMessageDialog(null,cipher.encryption1(),"文件加密成功",JOptionPane.INFORMATION_MESSAGE);//弹出消息提示框，展示加密后的文件内容
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        decryptFileButton.addActionListener(new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    JOptionPane.showMessageDialog(null, cipher.decryption1(), "解密文件成功", JOptionPane.INFORMATION_MESSAGE);//弹出消息提示框，展示解密后的文件内容
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("密钥:"));
+        panel.add(keyField);
+        panel.add(setKeyButton);
+        panel.add(new JLabel("明文:"));
+        panel.add(plaintextField);
+        panel.add(setPlaintextButton);
+        panel.add(new JLabel("密文:"));
+        panel.add(ciphertextField);
+        panel.add(setCiphertextButton);
+        panel.add(encryptButton);
+        panel.add(decryptButton);
+        panel.add(encryptFileButton);
+        panel.add(decryptFileButton);
+        panel.add(exitButton);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new CipherGUI();
+    }
+}
